0e6b43ea1040e6cb6da14e3f5765b6014633c8bf
msami0511 <mohamashaikh@algomau.ca>
2025-06-21 00:40:45 +0000

    User page updated to add createuser method

--- app/models/User.php
+++ app/models/User.php
@@ -7,7 +7,7 @@ class User {
     public $auth = false;
 
     public function __construct() {
-        
+
     }
 
     public function test () {
@@ -19,32 +19,37 @@ class User {
     }
 
     public function authenticate($username, $password) {
-        /*
-         * if username and password good then
-         * $this->auth = true;
-         */
-		$username = strtolower($username);
-		$db = db_connect();
+        $username = strtolower($username);
+        $db = db_connect();
         $statement = $db->prepare("select * from users WHERE username = :name;");
         $statement->bindValue(':name', $username);
         $statement->execute();
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
-		
-		if (password_verify($password, $rows['password'])) {
-			$_SESSION['auth'] = 1;
-			$_SESSION['username'] = ucwords($username);
-			unset($_SESSION['failedAuth']);
-			header('Location: /home');
-			die;
-		} else {
-			if(isset($_SESSION['failedAuth'])) {
-				$_SESSION['failedAuth'] ++; //increment
-			} else {
-				$_SESSION['failedAuth'] = 1;
-			}
-			header('Location: /login');
-			die;
-		}
+
+        if (password_verify($password, $rows['password'])) {
+            $_SESSION['auth'] = 1;
+            $_SESSION['username'] = ucwords($username);
+            unset($_SESSION['failedAuth']);
+            header('Location: /home');
+            die;
+        } else {
+            if(isset($_SESSION['failedAuth'])) {
+                $_SESSION['failedAuth'] ++; //increment
+            } else {
+                $_SESSION['failedAuth'] = 1;
+            }
+            header('Location: /login');
+            die;
+        }
     }
 
+    // ADD THIS METHOD:
+    public function getUserByUsername($username) {
+        $username = strtolower($username);
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * FROM users WHERE username = :username LIMIT 1");
+        $statement->bindValue(':username', $username);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
 }
