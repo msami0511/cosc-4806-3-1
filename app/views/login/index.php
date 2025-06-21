794307e9d2a3cd743f0801648cd97b86d9474f0d
msami0511 <mohamashaikh@algomau.ca>
2025-06-21 01:41:24 +0000

    Css file created and headerpublic file updated

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,29 +1,36 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-            </div>
-        </div>
-    </div>
+	<?php require_once 'app/views/templates/headerPublic.php' ?>
 
-<div class="row">
-    <div class="col-sm-auto">
-		<form action="/login/verify" method="post" >
-		<fieldset>
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input required type="text" class="form-control" name="username">
+	<main role="main" class="container">
+			<div class="page-header" id="banner">
+					<div class="row">
+							<div class="col-lg-12">
+									<h1>You are not logged in</h1>
+							</div>
+					</div>
 			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input required type="password" class="form-control" name="password">
+
+			<div class="row">
+					<div class="col-sm-auto">
+							<?php if (!empty($data['error'])): ?>
+									<div class="alert alert-danger"><?= htmlspecialchars($data['error']) ?></div>
+							<?php endif; ?>
+
+							<form action="/login/verify" method="post">
+									<fieldset>
+											<div class="form-group">
+													<label for="username">Username</label>
+													<input required type="text" class="form-control" name="username" id="username">
+											</div>
+											<div class="form-group">
+													<label for="password">Password</label>
+													<input required type="password" class="form-control" name="password" id="password">
+											</div>
+											<br>
+											<button type="submit" class="btn btn-primary">Login</button>
+									</fieldset>
+							</form>
+					</div>
 			</div>
-            <br>
-		    <button type="submit" class="btn btn-primary">Login</button>
-		</fieldset>
-		</form> 
-	</div>
-</div>
-    <?php require_once 'app/views/templates/footer.php' ?>
+	</main>
+
+	<?php require_once 'app/views/templates/footer.php' ?>
