7b9f7afcfe8b128ce68e89809492bacedea9e02b
msami0511 <mohamashaikh@algomau.ca>
2025-06-21 00:48:17 +0000

    index page updated to ensure sequence

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-require_once 'app/init.php';
+session_start();
+
+require_once 'app/core/App.php';
+require_once 'app/core/Controller.php';
+require_once 'app/core/config.php';
+require_once 'app/database.php';
 
 $app = new App;
