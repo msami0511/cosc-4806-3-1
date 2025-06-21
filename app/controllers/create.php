9bc2ec6189bbda99b2e79fc6aaf69723d088b716
msami0511 <mohamashaikh@algomau.ca>
2025-06-21 00:38:48 +0000

    Create page updated for new users to login

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -2,7 +2,24 @@
 
 class Create extends Controller {
 
-    public function index() {		
-	    $this->view('create/index');
+    public function index() {
+        $this->view('create/index');
+    }
+
+    public function createAccount() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'];
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+            $userModel = $this->loadModel('User');
+            if ($userModel->findUserByUsername($username)) {
+                $error = "Username already exists.";
+                $this->view('create/index', ['error' => $error]);
+                return;
+            }
+
+            $userModel->createUser($username, $password);
+            header("Location: /login");
+        }
     }
 }
